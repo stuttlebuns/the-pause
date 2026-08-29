2876eee07b2d8ec3cba94f21056e8e19185ec75c
stuttlebuns <stuttlebuns@gmail.com>
2026-08-29 12:33:37 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'the-pause-v1';
+const CACHE_NAME = 'the-pause-v2';
 const ASSETS = ['./', './index.html', './manifest.json'];
-
+ 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -16,9 +16,10 @@ self.addEventListener('activate', event => {
   );
   self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
+ 
